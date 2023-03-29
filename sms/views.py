32c6df1bf39cdd1851a4cdac0d80a609eafd8d90
Benard Mosomi <benardmosomi@gmail.com>
2023-03-29 18:45:41 +0300

    end if

--- sms/views.py
+++ sms/views.py
@@ -1977,8 +1977,8 @@ def disconnection_reminder(request, client_id):
     client_balance=str(client.amount_due)
     dear = "Dear "
     final = ", This is a final reminder to clear your outstanding water bill of Ksh."
-    payment = "Paybill 4108623, account "
-    disconnection = " by end of today. Please note that disconnection of normal supply will be done if we do not hear from you. AQUA NOVA Management. 0769721791"
+    payment = " Paybill 4108623, account "
+    disconnection = " by end of today. Please note that disconnection of normal supply will be done if we do not hear from you. AQUA NOVA Management- 0769721791"
     client_message = dear  +  client_name + final + client_balance + payment + account_client  + disconnection
        
     if request.method == 'POST':
